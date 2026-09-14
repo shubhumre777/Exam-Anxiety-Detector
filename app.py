07e2590576c4151be6777fbc0842871d95f5a324
Shubh Umre <umreshubh6@gmail.com>
2026-09-14 17:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,5 +97,6 @@ if uploaded_file is not None:
             
             final_score = analyzer.calculate_anxiety_score()
             st.info(f"🎯 **Final Exam Anxiety Score:** {final_score} / 100")
-            if final_score == 0 :
-                st.error("Please Upload the file again and Try Analyzing again") 
+
+            if final_score == 0:
+                st.error("Please upload the file again and try analyzing.")
